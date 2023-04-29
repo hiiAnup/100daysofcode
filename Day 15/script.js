205224157822b2bf7c2484e6f8e30eeb966c94b6
Anup Singh <129000754+hiiAnup@users.noreply.github.com>
2023-04-29 23:02:12 +0530

    Day 15

---
+++ Day 15/script.js
@@ -0,0 +1,17 @@
+function test() {
+    let a = "3";  //balty no 1  water
+    let b = "8";   // balty no 2  sand
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+let c =a;
+a=b;
+b=c;
+
+
+    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
